5854fac35b810f48a839ce25279de07c40e11ffc
Amanda Ubuntu VM2 <apiyapan@caltech.edu>
2020-05-30 18:11:12 -0700

    cleanup

--- include/demo_util.h
+++ include/demo_util.h
@@ -7,11 +7,14 @@
 #include "body.h"
 #include "vector.h"
 #include "list.h"
+#include "scene.h"
 
 body_t *make_box(double width, double height, rgb_color_t color, int player);
 
 body_t *make_trapezoid(double width, double height, double spacing, double slope, rgb_color_t color, int player);
 
 list_t *circle_sector(size_t rad, double min_rad, double max_rad);
+
+void scene_setup(scene_t *scene);
 
 #endif // #ifndef __DEMO_UTIL_H__
