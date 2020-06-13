c842a63715ce1f3b803c206f2bfea1557a1c332d
Juliette Koval <jkoval@caltech.edu>
2020-06-12 21:03:38 -0700

    Update sdl_wrapper.c

--- library/sdl_wrapper.c
+++ library/sdl_wrapper.c
@@ -233,7 +233,7 @@ void sdl_render_text(vector_t position, int font_size, char* text, rgb_color_t c
     
     TTF_CloseFont(font);
 }
-/*
+
 void sdl_render_image(vector_t position){
     assert(position.x >= 0 && position.x <= WINDOW_WIDTH);
     assert(position.y >= 0 && position.y <= WINDOW_HEIGHT);
@@ -251,7 +251,7 @@ void sdl_render_image(vector_t position){
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(image);
 }
-*/
+
 void sdl_show(void) {
     SDL_RenderPresent(renderer);
 }
